7c468a9245b15d05d12274d158f14bb331f979c0
ZhAngelova <126324371+ZhAngelova@users.noreply.github.com>
2024-04-15 23:57:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// const header = document.querySelector('.header');
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 600) {
+//         header.classList.add('scrolled');
+//     } else {
+//         header.classList.remove('scrolled'); // Optional: Remove the 'scrolled' class if scroll position is less than or equal to 50
+//     }
+// });
+
+
+
+
