9ca162ee0db9ccfb5512d1b17fae95cc8120215a
ZhAngelova <126324371+ZhAngelova@users.noreply.github.com>
2024-04-17 09:24:57 +0100

    Mobile Responsive Design added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// const header = document.querySelector('.header');
-
-// window.addEventListener('scroll', () => {
-//     if (window.scrollY > 600) {
-//         header.classList.add('scrolled');
-//     } else {
-//         header.classList.remove('scrolled'); // Optional: Remove the 'scrolled' class if scroll position is less than or equal to 50
-//     }
-// });
-
-
+// JavaScript to toggle navigation visibility when header is clicked
+// document.querySelector('header').addEventListener('click', function() {
+//     this.classList.toggle('clicked');
+//   });
+  
+// document.addEventListener('DOMContentLoaded', function () {
+//     const header = document.querySelector('header');
+//     const menuToggle = document.querySelector('.menu-toggle');
+  
+//     menuToggle.addEventListener('click', function () {
+//       header.classList.toggle('clicked');
+//     });
+//   });
+  
 
 
